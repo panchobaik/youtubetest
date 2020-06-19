b581cc88b7eeecf628fd827d2819aa5ced2964ec
makerPancho <maker.pancho.go@gmail.com>
2020-06-19 22:14:46 +0900

    Initial Commit

---
+++ youtubetest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  youtubetest
+//
+//  Created by 백승일 on 2020/06/19.
+//  Copyright © 2020 makerPancho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
